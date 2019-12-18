22360ff3f3f15e10aa02ca661b82cb1208b6a355
Daniel Roth <daroth@microsoft.com>
2019-12-18 10:01:38 -0800

    Initial

---
+++ src/BestForYouRecipes/JsonRecipesStore.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace BestForYouRecipes
+{
+    public class JsonRecipesStore : IRecipesStore
+    {
+        IDictionary<string, Recipe> recipes;
+
+        public JsonRecipesStore()
+        {
+            var json = File.ReadAllText("recipes.json");
+            var jsonOptions = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+                AllowTrailingCommas = true
+            };
+            recipes = JsonSerializer.Deserialize<Dictionary<string, Recipe>>(json, jsonOptions);
+        }
+
+        public Task<IEnumerable<Recipe>> GetRecipes()
+        {
+            return Task.FromResult<IEnumerable<Recipe>>(recipes.Values);
+        }
+    }
+}
---
+++ src/BestForYouRecipes/Recipe.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BestForYouRecipes
+{
+    public class Recipe
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Source { get; set; }
+        public int PrepTime { get; set; }
+        public int WaitTime { get; set; }
+        public int CookTime { get; set; }
+        public int Servings { get; set; }
+        public string Comments { get; set; }
+        public string Instructions { get; set; }
+        public string[] Ingredients { get; set; }
+        public string[] Tags { get; set; }
+
+        public Uri CardImageUrl
+        {
+            get
+            {
+                return new Uri($"images/cards/{Name}.png", UriKind.Relative);
+            }
+        }
+    }
+}
